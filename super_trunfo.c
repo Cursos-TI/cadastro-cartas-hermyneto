c4dc8676f31acacd15a458e19b7079e864b66b75
hermyneto <hermyneto@gmail.com>
2025-10-05 02:54:27 +0000

    Tema 3 Nivel Aventureiro

--- super_trunfo.c
+++ super_trunfo.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 
 int main() {
     char estado1, estado2;
@@ -11,6 +13,7 @@ int main() {
     float densidade_populacinal1, densidade_populacinal2;
     float pib_per_capita1, pib_per_capita2;
     float super_poder1, super_poder2;
+    int escolha_de_atributo;
 
     // Leitura Carta 1
     printf("Digite os dados da Carta 1:\n");
@@ -81,11 +84,11 @@ int main() {
     printf("Cidade: %s\n", cidade1);
     printf("Populacao: %lu\n", populacao1);
     printf("Area: %.2f km²\n", area1);
-    printf("PIB: %.d bilhões de reais\n", pib1);
+    printf("PIB: %.d reais\n", pib1);
     printf("Pontos turisticos: %d\n", pontos_turistico1);
-    printf("Densidade Populacinal: %2f hab/km²\n", densidade_populacinal1);
-    printf("PIB per Capita: %2f reais\n", pib_per_capita1);
-    printf("Super Poder: %2f\n", super_poder1);
+    printf("Densidade Populacinal: %.2f hab/km²\n", densidade_populacinal1);
+    printf("PIB per Capita: %.2f reais\n", pib_per_capita1);
+    printf("Super Poder: %.2f\n", super_poder1);
 
     // Exibição Carta 2
     printf("\nCarta 2:\n");
@@ -94,24 +97,131 @@ int main() {
     printf("Cidade: %s\n", cidade2);
     printf("Populacao: %lu\n", populacao2);
     printf("Area: %.2f km²\n", area2);
-    printf("PIB: %.d bilhões de reais\n", pib2);
+    printf("PIB: %.d reais\n", pib2);
     printf("Pontos turisticos: %d\n", pontos_turistico2);
-    printf("Densidade Populacinal: %2f hab/km²\n", densidade_populacinal2);
-    printf("PIB per Capita: %2f reais\n", pib_per_capita2);
+    printf("Densidade Populacinal: %.2f hab/km²\n", densidade_populacinal2);
+    printf("PIB per Capita: %.2f reais\n", pib_per_capita2);
     printf("Super Poder: %.2f\n\n", super_poder2);
 
-    // Comparação entre valores das cartas
-    printf("Comparação de Cartas(Atributo:população):\n");
-    printf("Carta 1 - %s: %d Habitantes\n", &cidade1, populacao1);
-    printf("Carta 2 - %s: %d Habitantes\n", &cidade2, populacao2);
+    // Escolha do Atributo
+    printf("\n### Super Trunfo: Comparacao de Cartas ###\n\n");
+    printf("Escolha o atributo para comparar:\n");
+    printf("1 - Populacao\n");
+    printf("2 - Area\n");
+    printf("3 - PIB\n");
+    printf("4 - Pontos Turisticos\n");
+    printf("5 - Densidade Populacinal\n");
+    printf("6 - PIB per Capita\n");
+    printf("7 - Super Poder\n");
+    printf("Opcao: ");
+    scanf("%d", &escolha_de_atributo);
 
-    if(populacao1 > populacao2){
-        printf("Carta 1 (%s) VENCEU!\n", cidade1);
-    }else if (populacao2 > populacao1){
-        printf("Carta 2 (%s) VENCEU!\n", cidade2);
-    } else {
-        printf("Empate!\n");
+    // Comparação dos atributos escolido
+    switch (escolha_de_atributo){
+    case 1:
+
+        printf("\nAtributo: Populacao\n");
+        printf("%s: %lu\n", cidade1, populacao1);
+        printf("%s: %lu\n", cidade2, populacao2);
+
+        if (populacao1 > populacao2){
+            printf("*** Você Venceu! ***\n");
+        }else if (populacao1 < populacao2){
+            printf("*** Você Perdeu! ***\n");
+        } else {
+            printf("*** Você Empatou! ***\n");
+        }
+        break;
+    case 2:
+
+        printf("\nAtributo: Area\n");
+        printf("%s: %.2f\n", cidade1, area1);
+        printf("%s: %.2f\n", cidade2, area2);
+
+        if (area1 > area2){
+            printf("*** Você Venceu! ***\n");
+        }else if (area1 < area2){
+            printf("*** Você Perdeu! ***\n");
+        } else {
+            printf("*** Você Empatou! ***\n");
+        }
+        break;
+    case 3:
+
+        printf("\nAtributo: PIB\n");
+        printf("%s: %d\n", cidade1, pib1);
+        printf("%s: %d\n", cidade2, pib2);
+
+        if (pib1 > pib2){
+            printf("*** Você Venceu! ***\n");
+        }else if (pib1 < pib2){
+            printf("*** Você Perdeu! ***\n");
+        } else {
+            printf("*** Você Empatou! ***\n");
+        }
+        break;
+    case 4:
+
+        printf("\nAtributo: Pontos turisticos\n");
+        printf("%s: %d\n", cidade1, pontos_turistico1);
+        printf("%s: %d\n", cidade2, pontos_turistico2);
+
+        if (pontos_turistico1 > pontos_turistico2){
+            printf("*** Você Venceu! ***\n");
+        }else if (pontos_turistico1 < pontos_turistico2){
+            printf("*** Você Perdeu! ***\n");
+        } else {
+            printf("*** Você Empatou! ***\n");
+        }
+        break;
+    case 5:
+
+        printf("\nAtributo: Densidade Populacional\n");
+        printf("%s: %.2f\n", cidade1, densidade_populacinal1);
+        printf("%s: %.2f\n", cidade2, densidade_populacinal2);
+
+        if (densidade_populacinal1 > densidade_populacinal2){
+            printf("*** Você Venceu! ***\n");
+        }else if (densidade_populacinal1 < densidade_populacinal2){
+            printf("*** Você Perdeu! ***\n");
+        } else {
+            printf("*** Você Empatou! ***\n");
+        }
+        break;
+    case 6:
+
+        printf("\nAtributo: PIB per Capita\n");
+        printf("%s: %.2f\n", cidade1, pib_per_capita1);
+        printf("%s: %.2f\n", cidade2, pib_per_capita2);
+
+        if (pib_per_capita1 > pib_per_capita2){
+            printf("*** Você Venceu! ***\n");
+        }else if (pib_per_capita1 < pib_per_capita2){
+            printf("*** Você Perdeu! ***\n");
+        } else {
+            printf("*** Você Empatou! ***\n");
+        }
+        break;
+    case 7:
+
+        printf("\nAtributo: Super Poder\n");
+        printf("%s: %.2f\n", cidade1, super_poder1);
+        printf("%s: %.2f\n", cidade2, super_poder2);
+
+        if (super_poder1 > super_poder2){
+            printf("*** Você Venceu! ***\n");
+        }else if (super_poder1 < super_poder2){
+            printf("*** Você Perdeu! ***\n");
+        } else {
+            printf("*** Você Empatou! ***\n");
+        }
+        break;
+    
+    default:
+        printf("\nOpçao Incorreta\n");
+        break;
     }
+
     
     
 
